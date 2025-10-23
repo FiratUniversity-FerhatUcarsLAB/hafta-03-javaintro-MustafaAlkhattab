55452f786048f1caa7439dd3dbc6b68f1c12b7d1
MustafaAlkhattab <250541607@firat.edu.tr>
2025-10-23 19:47:06 +0300

    CarpimTablosu.java

---
+++ Task-5-TabloDesenleri/CarpimTablosu.java
@@ -0,0 +1,17 @@
+public class CarpimTablosu {
+    public static void main(String[] args) {
+        // 2'nin Çarpım Tablosu 1'den 10'a Kadar
+        int number=2;
+        System.out.println("2'nin 1'den 10'a Kadar Çarpım Tablosu ");
+        System.out.println("2x1  = "+(number*1));
+        System.out.println("2x2  = "+(number*2));
+        System.out.println("2x3  = "+(number*3));
+        System.out.println("2x4  = "+(number*4));
+        System.out.println("2x5  = "+(number*5));
+        System.out.println("2x6  = "+(number*6));
+        System.out.println("2x7  = "+(number*7));
+        System.out.println("2x8  = "+(number*8));
+        System.out.println("2x9  = "+(number*9));
+        System.out.println("2x10 = "+(number*10));
+    }
+}
