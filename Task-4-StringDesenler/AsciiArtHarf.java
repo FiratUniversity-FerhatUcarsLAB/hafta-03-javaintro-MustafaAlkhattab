25e6832d8919d141fcfe36487b89c25bce79ed3a
MustafaAlkhattab <250541607@firat.edu.tr>
2025-10-23 19:40:36 +0300

    AsciiArtHarf.java

---
+++ Task-4-StringDesenler/AsciiArtHarf.java
@@ -0,0 +1,14 @@
+public class AsciiArtHarf {
+    public static void main (String[] args) {
+      // ASCII ART İLE C HARFİ YAZILIŞI
+        System.out.println ("    / / / / / / / / / ");
+        System.out.println ("  / ");
+        System.out.println (" / ");
+        System.out.println ("/ ");
+        System.out.println ("/ ");
+        System.out.println ("/ ");
+        System.out.println (" / ");
+        System.out.println ("  / ");
+        System.out.println ("   / / / / / / / / / ");
+    }
+}
