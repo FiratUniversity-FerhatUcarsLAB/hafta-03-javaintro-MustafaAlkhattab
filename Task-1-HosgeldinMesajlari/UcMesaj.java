67acd3f08fee32bdf98d8e2d4c926eb420c9daf6
MustafaAlkhattab <250541607@firat.edu.tr>
2025-10-23 19:21:29 +0300

    UcMesaj.java

---
+++ Task-1-HosgeldinMesajlari/UcMesaj.java
@@ -0,0 +1,8 @@
+public class UcMesaj {
+    public static void main(String[] args) {
+        System.out.println("Java'ya Hoş Geldiniz");
+        System.out.println("Bilgisayar Bilimlerine Hoşgeldin");
+        System.out.println("Programlama Eglencelidir");
+        System.out.println("");
+    }
+}
