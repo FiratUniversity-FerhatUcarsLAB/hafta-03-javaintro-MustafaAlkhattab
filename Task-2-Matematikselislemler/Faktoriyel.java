7bff81237c2f05542653f9582acdf6b89bc48a4e
MustafaAlkhattab <250541607@firat.edu.tr>
2025-10-23 19:27:24 +0300

    Faktoriyel.java

---
+++ Task-2-Matematikselislemler/Faktoriyel.java
@@ -0,0 +1,13 @@
+public class Faktoriyel {
+    public static void main (String[] args){
+        /**
+         *  5! Sonucunu Hesapla
+         * */
+        int faktoriyelSayi = 5 * 4 * 3 * 2 * 1;
+
+        /**
+         *  Sonucu Ekrana Yazdır
+         * */
+        System.out.println("5! Sonucu : "+faktoriyelSayi);
+    }
+}
